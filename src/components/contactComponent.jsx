0a2fa1fdeb2a7b83ac198cf745d6bbd517d8e8ac
Tanuja <tbanga1@students.towson.edu>
2020-04-27 03:56:20 -0400

    navigation to new service req page

--- src/components/contactComponent.jsx
+++ src/components/contactComponent.jsx
@@ -4,14 +4,18 @@ class ContactComponent extends Component {
   state = {};
   render() {
     return (
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d76765.98321148289!2d-73.96694563267306!3d40.751663750099084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1525939514494"
-        title="This is a unique title"
-        width="100%"
-        height="100%"
-        frameBorder="0"
-        style={{ border: 0 }}
-      />
+      <React.Fragment>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3083.478496632825!2d-76.60816778463499!3d39.3906793794976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c80fa2cf86a6a9%3A0x36774b98ccc72a6d!2s7800%20York%20Rd%2C%20Towson%2C%20MD%2021252!5e0!3m2!1sen!2sus!4v1587974067151!5m2!1sen!2sus"
+          width="600"
+          height="450"
+          frameborder="0"
+          style="border:0;"
+          allowfullscreen=""
+          aria-hidden="false"
+          tabindex="0"
+        ></iframe>
+      </React.Fragment>
     );
   }
 }
