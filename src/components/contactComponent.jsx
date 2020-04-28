df201444af48d6b26cbc2df70833fedba8247455
Tanuja <tbanga1@students.towson.edu>
2020-04-27 22:08:28 -0400

    navigation to new service req page

--- src/components/contactComponent.jsx
+++ src/components/contactComponent.jsx
@@ -5,16 +5,40 @@ class ContactComponent extends Component {
   render() {
     return (
       <React.Fragment>
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3083.478496632825!2d-76.60816778463499!3d39.3906793794976!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89c80fa2cf86a6a9%3A0x36774b98ccc72a6d!2s7800%20York%20Rd%2C%20Towson%2C%20MD%2021252!5e0!3m2!1sen!2sus!4v1587974067151!5m2!1sen!2sus"
-          width="600"
-          height="450"
-          frameborder="0"
-          style="border:0;"
-          allowfullscreen=""
-          aria-hidden="false"
-          tabindex="0"
-        ></iframe>
+        <div class="row">
+          <div class="col-md-1"></div>
+          <div class="col-md-5 mb-4 hours">
+            <h3>
+              <span class="fa fa-clock-o"></span> Hours of Operation
+            </h3>
+            <br />
+            <p>Monday: 8:00 am - 6:00 pm</p>
+            <p>Tuesday: 8:00 am - 6:00 pm</p>
+            <p>Wednesday: 8:00 am - 6:00 pm</p>
+            <p>Thursday: 8:00 am - 6:00 pm</p>
+            <p>Friday: 8:00 am - 6:00 pm</p>
+            <p>Saturday: 8:00 am - 4:00 pm</p>
+            <p>Sunday: Closed</p>
+          </div>
+
+          <div class="col-md-6 mb-4 map">
+            <h3>
+              <span class="fa fa-map"></span>Maps
+            </h3>
+            <br />
+            <div class="map">
+              <iframe
+                width="500"
+                height="450"
+                frameborder="0"
+                style="border:0"
+                src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJLX2exqIPyIkRXmWz1aR-K6I&key=AIzaSyCc-O4BAlkPlieOJYfB2ovpHxEMu27ajPA"
+                allowfullscreen
+              ></iframe>
+            </div>
+          </div>
+          <div class="col-md-1"></div>
+        </div>
       </React.Fragment>
     );
   }
